70bdafe42273e94e0cbe1191665db2da3149cfb5
zaquille-oneil <zakglee@gmail.com>
2017-07-27 15:51:08 -0700

    mobile help..

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,8 +4,8 @@ import '../App.css';
 class Banner extends Component {
    render() {
       return (
-         <div className="App-header">
-            <span>NBA - 1 on 1</span>
+         <div className="App-header" style={{whiteSpace: 'nowrap'}}>
+            <div>NBA - 1 on 1</div>
          <span style={{fontSize: 'small', color: '#004E85', backgroundColor: '#5CA0D0'}}> choose two players and find out who wins.
                    <br/> by zacchias - built with react, javascript, bootstrap</span>
          </div>
